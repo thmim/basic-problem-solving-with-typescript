fe9a8bfb3860b3bdb3c2255cf72bed43b277363d
thmim <mdtaharimmim1010@gmail.com>
2026-05-27 15:28:30 +0600

    another problem solution include

--- src/solutions.ts
+++ src/solutions.ts
@@ -1,52 +1,100 @@
 
-// function filterEvenNumbers(numbersArray: number[]) {
-//     const evenNumbersArray: number[] = [];
-//     for (const num of numbersArray) {
+function filterEvenNumbers(numbersArray: number[]) {
+    const evenNumbersArray: number[] = [];
+    for (const num of numbersArray) {
 
-//         if (num % 2 === 0) {
-//             evenNumbersArray.push(num)
-//         }
-//     }
-//     return evenNumbersArray;
+        if (num % 2 === 0) {
+            evenNumbersArray.push(num)
+        }
+    }
+    return evenNumbersArray;
 
-// }
-// const result = filterEvenNumbers([11, 12, 13, 14, 15, 61, 72, 18])
-// console.log(result)
+}
 
 
-// function reverseString (input:string){
-//     let reverseLetter:string = "";
 
-//     for(let i=input.length-1;i>=0;i--){
-//        reverseLetter = reverseLetter + input[i]
-//     }
-//     return reverseLetter;
-// }
-//  const result = reverseString("abcdefg")
-//  console.log(result)
+function reverseString (input:string){
+    let reverseLetter:string = "";
 
-//  function checkType (input: string | number){
-//     if(typeof input === "number" ){
-//         return "Number"
-//     }
-//     else{
-//         return "String"
-//     }
+    for(let i=input.length-1;i>=0;i--){
+       reverseLetter = reverseLetter + input[i]
+    }
+    return reverseLetter;
+}
 
-//  }
 
-//  const result = checkType(23)
-//  console.log(result)
+ function checkType (input: string | number){
+    if(typeof input === "number" ){
+        return "Number"
+    }
+    else{
+        return "String"
+    }
+
+ }
+
+
 
 
 function getProperty<T, K extends keyof T>(obj: T, key: K){
     return obj[key];
 }
 
- const user = { id: 1, name: "John Doe", age: 21 };
-
- const result = getProperty(user, "age");
- console.log(result)
 
 
- 
+
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+
+function toggleReadStatus(book: Book) {
+    return {
+        ...book,
+        isRead: true
+    };
+}
+
+
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    grade: string;
+
+    constructor(name: string, age: number, grade: string) {
+        super(name, age)
+        this.grade = grade;
+    }
+
+    getDetails() {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+
+
+
+function getIntersection(array1: number[], array2: number[]): number[] {
+    const matchedNumber: number[] = [];
+    for (const number of array1) {
+        const findNumber = number;
+
+        if (array2.includes(findNumber)) {
+            matchedNumber.push(findNumber)
+        }
+    }
+    return matchedNumber;
+}
+
+
+
